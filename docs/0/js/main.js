8b0ddc673e14665dbafc52675247a6a5f5164247
ytyaru <yttry0@gmail.com>
2025-12-22 12:59:52 +0900

    Created a repository

---
+++ docs/0/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru';
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.Typed.20251222110655/`}, 'Typed')),
+        van.tags.p('型を定める。特にNumber型。'),
+//        van.tags.p('Determine the type. Especially the Number type.'),
+    );
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
+
+    const a = new Assertion();
+    a.t(true);
+    a.f(false);
+    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
+    a.fin();
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
